c5e57bc8bec4ff805b970b9b576573098cac4a57
Jatin Beohar <jatinbeohar@gmail.com>
2017-02-08 23:16:54 +0530

    Create 5input.cpp

---
+++ 5input.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+
+int main(void)
+{   int a,b,c,d,e,i,count=0;
+	std::cin>>a>>b>>c>>d>>e;
+	int A[a], B[b], C[c], D[d], E[e];
+	for (i=0; i<a; i++)
+		{std:: cin>>A[i];
+		}
+	for (i=0; i<b; i++)
+		{std:: cin>>B[i];
+		}
+	for (i=0; i<c; i++)
+		{std:: cin>>C[i];
+		}
+	for (i=0; i<d; i++)
+		{std:: cin>>D[i];
+		}
+	for (i=0; i<e; i++)
+		{std:: cin>>E[i];
+		}
+    return 0;
+}
